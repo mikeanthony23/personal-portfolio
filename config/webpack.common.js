dac295c40978993e816dd94f73500088a7df6291
Mike Anthony <mikeanthony595@gmail.com>
2024-04-28 15:46:36 +0800

    updated public folder build path

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -26,7 +26,7 @@ module.exports = {
       patterns: [
         {
           from: paths.public,
-          to: 'assets',
+          to: '',
           globOptions: {
             ignore: ['*.DS_Store'],
           },
